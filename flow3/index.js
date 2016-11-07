3923aa83debc1f44a2f428a78e89238b8ff8591d
Zarkoix <adam.towers@me.com>
2016-11-06 18:08:20 -0800

    update to flow3

--- flow3/index.js
+++ flow3/index.js
@@ -2,48 +2,85 @@ var flow3App = angular.module('flow3App', [])
 
 
 flow3App.controller('ngFlow3', function ngFlow3($scope) {
-  console.log('idk')
   $scope.flow = []
-  $scope.addArg = function() {
-    console.log('heheXD')
-    $scope.flow.push([[{"type":"extension", "text": ""}]])
-  }
 
-  $scope.extend = function(index) {
-    $scope.flow[index].push({"type": "extension", "text": ""})
-  }
-
-  $scope.respond = function(index) {
-    $scope.flow[index].push({"type": "response", "text": ""})
-  }
-
-  $scope.arrow = function(index) {
-    $scope.flow[index].push({"type": "arrow"})
+  $scope.newContention = function() {
+    $scope.flow.push({"title": "", "args": []})
   }
 })
 
 flow3App.directive('box', function() {
   return {
     restrict: 'E',
-    link: function(scope, element, attr) {
-      console.log(scope.p)
-      if(scope.p.type == 'arrow') {
-        element.children().prepend('<img class="arrow" src="arrow.svg">')
-      }
-
-      element.children().addClass(color(scope.p.type))
-
-      function color(type) {
+    scope: {
+      title: '=',
+      text: '=',
+      type: '=',
+      index: '=',
+      remove: '&'
+    },
+    controller: function () {
+      this.color = function (type) {
         if(type == 'extension') return 'blue accent-1'
         if(type == 'response') return 'red accent-1'
         if(type == 'arrow') return 'green accent-1'
         return ''
       }
 
-      scope.remove = function (x){
-        scope.arg.splice(x, 1)
+      this.rm = function () {
+        this.remove({index: this.index});
       }
     },
+    controllerAs: 'b',
+    bindToController: true,
     templateUrl: 'box.html'
+  }
+})
+
+flow3App.directive('arguement', function() {
+  return {
+    restrict: 'E',
+    transclude: true,
+    scope: {
+      boxes: '='
+    },
+    controller: function () {
+      this.extend = function() {
+        this.boxes.push({"type": "extension", "text": ""})
+      }
+
+      this.respond = function() {
+        this.boxes.push({"type": "response", "text": ""})
+      }
+
+      this.arrow = function() {
+        this.boxes.push({"type": "arrow"})
+      }
+
+      this.remove = function(index) {
+        this.boxes.splice(index, 1)
+      }
+    },
+    controllerAs: 'a',
+    bindToController: true,
+    templateUrl: 'arg.html'
+  }
+})
+
+flow3App.directive('contention', function() {
+  return {
+    restrict: 'E',
+    scope: {
+      args: '=',
+      title: '='
+    },
+    controller: function () {
+      this.newArg = function () {
+        this.args.push([{"title": "", "text": "", "type": "constructive"}])
+      }
+    },
+    controllerAs: 'c',
+    bindToController: true,
+    templateUrl: 'contention.html'
   }
 })
